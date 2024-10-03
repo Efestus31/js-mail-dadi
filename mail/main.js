f681959d1a0aec273863498efad9cadf68298b87
Efestus31 <biagiohd@gmail.com>
2024-10-03 16:56:27 +0200

    fine esercizio email

--- mail/main.js
+++ mail/main.js
@@ -2,9 +2,30 @@
 
 
 //creo una lista usando un array
+let email_autorizzate = [
+    "mario.rossi@gmail.com",
+    "lucia.bianchi@gmail.com",
+    "carlo.verdi@gmail.com",
+    "anna.gialli@gmail.com",
+    "marco.neri@gmail.com",
+    "biagiohd@gmail.com"
+];
+let trovata = false;
+
 
 //prompt utente per inserire la propria mail
+let email_da_autorizzare =prompt("ciao, inserisci la tua mail da verificare!");
 
 //verifico che l'email fornita dall'utente sia nella lista
-
+for(let i = 0; i < email_da_autorizzare.length; i++){
+    if(email_autorizzate[i] === email_da_autorizzare){
+        trovata = true; //email trovata
+        break; //esci dal ciclo
+    }
+}
 //messaggio di conferma
+if(trovata){
+    alert("accesso consentito!");
+} else{
+    alert("accesso negato!");
+}
