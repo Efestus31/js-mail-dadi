0b333c8966f26219a5fde53e8d1661ee5f95a32d
Efestus31 <biagiohd@gmail.com>
2024-10-03 16:29:09 +0200

    initial commit

---
+++ dadi/main.js
@@ -0,0 +1,11 @@
+/*
+Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
+*/
+
+//inizializzare le variabili
+
+//funzione math.random
+
+//funzione i/elseif/else per confontare i numeri e stabilire quale sia quello maggiore
+//all'interno della funzione dichiariamo anche il vincitore
