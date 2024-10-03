1da86db45d5d9ce50be76d7dd1ada8d28b9d64ee
Efestus31 <biagiohd@gmail.com>
2024-10-03 16:37:40 +0200

    gioco dadi completo

--- dadi/main.js
+++ dadi/main.js
@@ -4,8 +4,23 @@ Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. St
 */
 
 //inizializzare le variabili
-
+let player = Math.floor(Math.random() * 6) + 1;
+let computer = Math.floor(Math.random() * 6) + 1;
 //funzione math.random
 
 //funzione i/elseif/else per confontare i numeri e stabilire quale sia quello maggiore
 //all'interno della funzione dichiariamo anche il vincitore
+if(player > computer){
+    console.log('Vince il giocatore!'); 
+    console.log(player, computer);
+       
+} else if(computer > player) {
+    console.log('Vince il computer!');
+    console.log(computer, player);
+    
+    
+} else{
+    console.log('Parità');
+    console.log(computer, player);
+    
+}
