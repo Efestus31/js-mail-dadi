2fdba170bbb0c50d774ff5053497985d21a6b36f
Efestus31 <biagiohd@gmail.com>
2024-10-03 16:44:22 +0200

    initial commit mail

---
+++ mail/main.js
@@ -0,0 +1,10 @@
+//Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.  
+
+
+//creo una lista usando un array
+
+//prompt utente per inserire la propria mail
+
+//verifico che l'email fornita dall'utente sia nella lista
+
+//messaggio di conferma
